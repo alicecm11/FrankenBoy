83288fd4d57988d669de6520c73cb057943fd91f
alicecm11 <63906571+alicecm11@users.noreply.github.com>
2020-04-18 15:25:44 +0100

    Add files via upload

---
+++ PowerUpsBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpsBehaviour : MonoBehaviour
+
+{
+    public GameObject powerup;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousepos = Input.mousePosition;
+            mousepos.z = 2.0f;
+            mousepos.y = Screen.height;
+            Vector3 poweruppos = Camera.main.ScreenToWorldPoint(mousepos);
+            Instantiate(powerup, poweruppos,Quaternion.identity,transform);
+            Debug.Log("Mouse Left pressed at" + (poweruppos));
+        }
+
+        
+            
+    }
+}
+
+        
+
+
